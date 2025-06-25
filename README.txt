b4445d28a3b2d09396b1411335754de3ac75783f
Kamran Asadov <kamraasadov212@gmail.com>
2025-06-25 17:28:36 +0400

    Add files via upload
    This is a simple command-line calculator built in Python as part of my early learning journey. It performs basic arithmetic operations like addition, subtraction, multiplication, and division based on user input.
    
    I built this project to understand how functions, conditionals, and user input work in Python. It helped me practice clean coding, logic structuring, and error-free execution.
    
    This calculator runs directly in the terminal and is perfect for beginners who want to understand how to handle basic inputs and outputs in Python.
    
    Note: The Calculator does round to the nearest 4 d.p.

---
+++ README.txt
@@ -0,0 +1,12 @@
+# Python Calculator
+
+ A simple command-line calculator built in Python.
+
+## Features
+- Addition, subtraction, multiplication, division
+- Basic input/output
+- Beginner project to learn Python logic
+
+## How to Run
+1. Install Python
+2. Run `python calculator.py` in terminal
---
+++ calculator.py
@@ -0,0 +1,19 @@
+
+operator = input("Enter a operator(+ - * /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1 / num2
+    print(result)
+else:
+    print(f"{operator} is not a valid operator")
